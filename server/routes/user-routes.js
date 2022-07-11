62c8f668b0445e3f709a290ac36515731f5ff2d6
Brian Samuels <samuels.brian9@gmail.com>
2022-07-11 13:47:06 -0400

    adding in post requests for user thoughts. Working.

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -20,7 +20,56 @@ router.get('/users', (req, res) => {
       } else {
         res.json(data.Items);
       }
-    });
   });
+});
 
-  module.exports = router;
+// More to come
+router.get('/users/:username', (req, res) => {
+    console.log(`Querying for thought(s) from ${req.params.username}.`);
+    const params = {
+      TableName: table,
+      KeyConditionExpression: '#un = :user',
+      ExpressionAttributeNames: {
+        '#un': 'username',
+        '#ca': 'createdAt',
+        '#th': 'thought',
+      },
+      ExpressionAttributeValues: {
+        ':user': req.params.username,
+      },
+      ProjectionExpression: '#th, #ca',
+      ScanIndexForward: false,
+    };
+    dynamodb.query(params, (err, data) => {
+      if (err) {
+        console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+        res.status(500).json(err); // an error occurred
+      } else {
+        console.log("Query succeeded.");
+        res.json(data.Items)
+      }
+    });
+}); // closes the route for router.get(users/:username)
+
+// Create new user at /api/users
+router.post('/users', (req, res) => {
+  const params = {
+    TableName: table,
+    Item: {
+      username: req.body.username,
+      createdAt: Date.now(),
+      thought: req.body.thought,
+    },
+  };
+  dynamodb.put(params, (err, data) => {
+    if (err) {
+      console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+      res.status(500).json(err); // an error occurred
+    } else {
+      console.log("Added item:", JSON.stringify(data, null, 2));
+      res.json({"Added": JSON.stringify(data, null, 2)});
+    }
+  });
+});  // ends the route for router.post('/users')
+
+module.exports = router;
